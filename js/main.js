f8fbe6730a73e2ed7fbe50b1b65903aef7815624
Imtiajul Islam <iimtiajul@gmail.com>
2019-08-18 21:57:11 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+//Active Nav Code 
+jQuery(document).ready(function () {
+    $(function () {
+        $('.bxslider').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 1520
+        });
+    });
+});
+
